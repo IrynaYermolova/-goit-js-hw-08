be9db7ccff214370212a76546051b0a189a714bc
IrynaYermolova <ermolova_ira@ukr.net>
2023-03-01 11:09:26 +0200

    change

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,86 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
+console.log(SimpleLightbox);
 
-console.log(galleryItems);
+
+
+
+// const galleryImgContainer = document.querySelector(".gallery");
+// const markupImg = createMarkupImg(galleryItems);
+
+// galleryImgContainer.insertAdjacentHTML("beforeend", markupImg);
+// galleryImgContainer.addEventListener("click", galleryImgContainerClick);
+
+// function createMarkupImg(galleryItems) {
+//   return galleryItems
+//     .map(({ preview, original, description }) => {
+//       return `<div class="gallery__item">
+//          <a class="gallery__link" href="${original}">
+//            <img
+//              class="gallery__image"
+//              src="${preview}"
+//              data-source="${original}"
+//             alt="${description}"
+//            />
+//          </a>
+//        </div> `;
+//     })
+//     .join("");
+// }
+
+// function galleryImgContainerClick(evt) {
+//   evt.preventDefault();
+//   if (evt.target.nodeName !== "IMG") {
+//     return;
+//   }
+
+//   const currentImgUrl = evt.target.dataset.source;
+
+//   const instance = basicLightbox.create(
+//     `
+// 		<img src="${currentImgUrl}" width="1280" height="auto"/>
+//         `,
+//     {
+//       onShow: (instance) => {
+//        document.addEventListener("keydown", onEscKey);
+//       },
+//       onClose: (instance) => {
+//         document.removeEventListener("keydown", onEscKey);
+//       },
+//     }
+//   );
+//   instance.show();
+
+//   function onEscKey(evt) {
+//     if (evt.code !== "Escape") return;
+//     instance.close();
+//   }
+// }
+// console.log(galleryItems);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(galleryItems);
