def805d968447ca3c3f9b495dac199f314b4e1fe
IrynaYermolova <ermolova_ira@ukr.net>
2023-03-01 12:00:23 +0200

    save_task_01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,86 +1,41 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
+// import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 console.log(SimpleLightbox);
 
 
 
 
-// const galleryImgContainer = document.querySelector(".gallery");
-// const markupImg = createMarkupImg(galleryItems);
+const galleryImgContainer = document.querySelector(".gallery");
+const markupImg = createMarkupImg(galleryItems);
 
-// galleryImgContainer.insertAdjacentHTML("beforeend", markupImg);
-// galleryImgContainer.addEventListener("click", galleryImgContainerClick);
+galleryImgContainer.insertAdjacentHTML("beforeend", markupImg);
+galleryImgContainer.addEventListener("click", galleryImgContainerClick);
 
-// function createMarkupImg(galleryItems) {
-//   return galleryItems
-//     .map(({ preview, original, description }) => {
-//       return `<div class="gallery__item">
-//          <a class="gallery__link" href="${original}">
-//            <img
-//              class="gallery__image"
-//              src="${preview}"
-//              data-source="${original}"
-//             alt="${description}"
-//            />
-//          </a>
-//        </div> `;
-//     })
-//     .join("");
-// }
+function createMarkupImg(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<ul class="gallery__item">
+      <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+       </ul> `;
+    })
+    .join("");
+}
+function galleryImgContainerClick(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+}
 
-// function galleryImgContainerClick(evt) {
-//   evt.preventDefault();
-//   if (evt.target.nodeName !== "IMG") {
-//     return;
-//   }
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
 
-//   const currentImgUrl = evt.target.dataset.source;
-
-//   const instance = basicLightbox.create(
-//     `
-// 		<img src="${currentImgUrl}" width="1280" height="auto"/>
-//         `,
-//     {
-//       onShow: (instance) => {
-//        document.addEventListener("keydown", onEscKey);
-//       },
-//       onClose: (instance) => {
-//         document.removeEventListener("keydown", onEscKey);
-//       },
-//     }
-//   );
-//   instance.show();
-
-//   function onEscKey(evt) {
-//     if (evt.code !== "Escape") return;
-//     instance.close();
-//   }
-// }
-// console.log(galleryItems);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// console.log(galleryItems);
+console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,6 @@
 import Player from '@vimeo/player';
 
+
+
+
 console.log(Player);
