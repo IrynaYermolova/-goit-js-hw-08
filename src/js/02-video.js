a2005e00d97dacbd058467d038d3a947129a855d
IrynaYermolova <ermolova_ira@ukr.net>
2023-03-04 20:54:11 +0200

    add task-02,task-03

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,18 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+let savedTime = localStorage.getItem('videoplayer-current-time');
 
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
 
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 1000)
+);
 
-console.log(Player);
+console.log(player);
